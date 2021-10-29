8f5822037913eb117a2c6d50b14e841cf6964ac9
Okawa <pandanyanya@gmail.com>
2021-10-29 12:37:37 +0900

    コントローラーの修正

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,2 +1,31 @@
 class ReservesController < ApplicationController
+
+  before_action :permit_params, except: :new
+
+	def new
+		@reserve = Reserve.new
+	end
+
+	def back
+		@reserve = Reserve.new(@attr)
+		render :new
+	end
+
+	def confirm
+		@reserve = Reserve.new(@attr)
+		if @reserve.invalid?
+			render :new
+		end
+	end
+
+	def complete
+		Reserve.create!(@attr)
+	end
+
+	private
+
+	def permit_params
+		@attr = params.require('reserve').permit(:id, :reserve_date, :name, :note)
+	end
+
 end
