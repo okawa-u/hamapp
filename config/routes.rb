ab056a7dfd6d5b8a5082430584f993d642779e4e
Okawa <pandanyanya@gmail.com>
2021-10-29 12:36:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  get 'reserve/new' # 入力画面
+  post 'reserve/confirm' # 確認画面
+  post 'reserve/back' # 確認画面から「入力画面に戻る」をクリックしたとき
+  post 'reserve/complete' # 完了画面
 end
