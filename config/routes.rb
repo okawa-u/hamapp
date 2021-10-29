145f4b247044aa5a598678fecf25d05b54caf235
Okawa <pandanyanya@gmail.com>
2021-10-29 13:29:19 +0900

    ルーティングのコントローラー名修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'reserve/new' # 入力画面
-  post 'reserve/confirm' # 確認画面
-  post 'reserve/back' # 確認画面から「入力画面に戻る」をクリックしたとき
-  post 'reserve/complete' # 完了画面
+  get 'reserves/new' # 入力画面
+  post 'reserves/confirm' # 確認画面
+  post 'reserves/back' # 確認画面から「入力画面に戻る」をクリックしたとき
+  post 'reserves/complete' # 完了画面
 end
