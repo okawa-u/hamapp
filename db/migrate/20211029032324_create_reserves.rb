c8ab3739af94690b233483eb3b612c1d17113d07
Okawa <pandanyanya@gmail.com>
2021-10-29 12:29:45 +0900

    マイグレーションファイルの記述

--- db/migrate/20211029032324_create_reserves.rb
+++ db/migrate/20211029032324_create_reserves.rb
@@ -1,7 +1,9 @@
 class CreateReserves < ActiveRecord::Migration[6.0]
   def change
     create_table :reserves do |t|
-
+      t.string :reserve_date
+      t.string :name
+      t.string :note
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_29_032324) do
+
+  create_table "reserves", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "reserve_date"
+    t.string "name"
+    t.string "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
